704fb8f21e015f579aaf29a77ec69bb5405c662e
HydraGriua <f.saavedra1810@gmail.com>
2020-08-22 13:01:43 -0500

    Cambio en algunos algoritmos

--- Clases/SortingGrafico.py
+++ Clases/SortingGrafico.py
@@ -1,47 +1,43 @@
-import numpy as np
 import random
 import time
 from matplotlib import pyplot as plt
 
 # Bubble sort   
-def bubbleSort(v): 
-    n = len(v)  
-    for i in range(n): 
-        for j in range(0, n-i-1): 
-            if v[j] > v[j+1] : 
-                v[j], v[j+1] = v[j+1], v[j]     
+def Bubblesort(v): 
+  n, intercambia, x = len(v), True, -1
+  while intercambia:
+    intercambia, x = False, x + 1
+    for i in range(1, n-x):
+      if v[i-1] > v[i]:
+        v[i-1], v[i] = v[i], v[i-1]
+        intercambia = True   
 
 
 # Insertion sort
-def insertionSort(v):
-
-    for step in range(1, len(v)):
-        key = v[step]
-        j = step - 1
-        
-        # Compare key with each element on the left of it until an element smaller than it is found
-        # For descending order, change key<v[j] to key>v[j].        
-        while j >= 0 and key < v[j]:
-            v[j + 1] = v[j]
-            j = j - 1
-        
-        # Place key at after the element just smaller than it.
-        v[j + 1] = key
+def Insertionsort(arr):
+    n = len(arr)
+    for i in range(1,n):
+        key = arr[i]
+        j = i-1
+        while j>=0 and arr[j] > key:
+            arr[j+1]=arr[j]
+            j = j-1
+        arr[j+1] = key
 
 
 # Selection sort
-def selectionSort(v):
-   
-    for step in range(len(v)-1):
-        min_idx = step
-        for i in range(step + 1, len(v)-1):
-            if v[i] < v[min_idx]:
-                min_idx = i
-        (v[step], v[min_idx]) = (v[min_idx], v[step])
+def Selectionsort(arr):
+    n = len(arr)
+    for i in range(0,n-1):
+        min = i
+        for j in range(i+1,n):
+            if arr[j] < arr[min]:
+                min = j
+        arr[i], arr[min] = arr[min],arr[i]
 
 
 # Quick sort
-def quickSort(v):
+def Quicksort(v):
    quickSortHelper(v,0,len(v)-1)
 
 def quickSortHelper(v,first,last):
@@ -147,7 +143,7 @@ def GenVec():
   w = random.sample(range(1000001),100)
   q = random.sample(range(1000001),250)
   p = random.sample(range(1000001),500)
-  r = random.sample(range(1000001),800)
+  r = random.sample(range(1000001),1000)
   #t = random.sample(range(1000001),10000)
   #u = random.sample(range(1000001),100000)
   #c = random.sample(range(1000001),1000000)
@@ -163,33 +159,33 @@ Merge = []
 Quick = []
 Heap = []
 
-B = A.copy()
+B = A.copy() 
+C = A.copy() 
+D = A.copy() 
+E = A.copy() 
+F = A.copy() 
+G = A.copy() 
 for i in B:
   measure_time(Mergesort, i,Merge)
-B = A.copy()
-for i in B:
-  measure_time(insertionSort, i,Insertion)
-B =A.copy()
-for i in B:
-  measure_time(bubbleSort, i,Bubble)
-B = A.copy()
-for i in B:
-  measure_time(quickSort, i,Quick)
-B = A.copy()
-for i in B:
-  measure_time(selectionSort, i,Selection)
-B = A.copy()
-for i in B:
+for i in C:
+  measure_time(Insertionsort, i,Insertion)
+for i in D:
+  measure_time(Bubblesort, i,Bubble)
+for i in E:
+  measure_time(Quicksort, i,Quick)
+for i in F:
+  measure_time(Selectionsort, i,Selection)
+for i in G:
   measure_time(Heapsort, i,Heap)
 
 
 
 
 #X: elementos en el arr
-arr = [5, 10, 50, 100, 250, 500, 800]#, 10000, 100000, 1000000]
+arr = [5, 10, 50, 100, 250, 500, 1000]#, 10000, 100000, 1000000]
 
 
-#Lineas 
+# Lineas 
 plt.plot(arr,Bubble)
 plt.plot(arr,Insertion)
 plt.plot(arr,Selection)
