3e76682def84cccb1d3000352c6e95c9a0e9b3a7
HydraGriua <f.saavedra1810@gmail.com>
2020-08-27 20:06:04 -0500

    Se añade la primera version de Anillo de Primos

---
+++ Clases/AnilloPrimos.py
@@ -0,0 +1,40 @@
+def Anillo(H,n,solucion):
+    if H == n:
+        print(solucion)
+        return
+    else:
+        for i in range(0,n):
+            solucion[H] = i+1
+        
+            sig = True
+
+            for i in range(H):
+                if solucion[H] == solucion[i]:
+                    sig = False
+                    break
+            
+            if H>0:
+                X = solucion[H] + solucion[H-1]
+                if H+1 == n:
+                    X = solucion[H] + solucion[0]
+                cont=0
+                for i in range(1,X+1):
+                    if X % i == 0:
+                        cont+=1
+                if cont > 2:
+                    sig = False
+            
+            if sig:
+                Anillo(H+1,n,solucion)
+
+
+n = 9
+sol = [-1]*n
+Anillo(0,n,sol)
+            
+
+        
+            
+        
+
+        
