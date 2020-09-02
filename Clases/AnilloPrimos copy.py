2de6a7c01d97da6d099ad936d2f44190f4487f81
HydraGriua <f.saavedra1810@gmail.com>
2020-09-01 21:08:02 -0500

    Segunda version de Anillo de Primos

--- Clases/AnilloPrimos copy.py
+++ Clases/AnilloPrimos copy.py
@@ -15,20 +15,24 @@ def Anillo(H,n,solucion):
             
             if H>0:
                 X = solucion[H] + solucion[H-1]
-                if H+1 == n:
-                    X = solucion[H] + solucion[0]
                 cont=0
+                cont2=0
                 for i in range(1,X+1):
                     if X % i == 0:
                         cont+=1
-                if cont > 2:
+                if H+1 == n:
+                    X = solucion[H] + solucion[0]
+                    for i in range(1,X+1):
+                        if X % i == 0:
+                            cont2+=1
+                if cont > 2 or cont2 > 2:
                     sig = False
-            
+               
             if sig:
                 Anillo(H+1,n,solucion)
 
 
-n = 9
+n = 8
 sol = [-1]*n
 Anillo(0,n,sol)
             
